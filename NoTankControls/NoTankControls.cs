bd624d52fae7e7a0c1f1647e950af2679dee9442
Gyztor <odovakiin34@gmail.com>
2026-07-22 17:16:15 -0500

    modernize the buildin and csproj. Add Nytra's toggle for the mod.

--- NoTankControls/NoTankControls.cs
+++ NoTankControls/NoTankControls.cs
@@ -6,27 +6,34 @@ using FrooxEngine.UIX;
 
 namespace NoTankControls
 {
-    public class XyMod : ResoniteMod
+    public class NoTankControls : ResoniteMod
     {
         public override string Name => "NoTankControls";
         public override string Author => "zyntaks";
-        public override string Version => "1.0.0";
-        public override string Link => "https://github.com/furrz/NoTankControls";
+        public override string Version => "2.0.0";
+        public override string Link => "https://github.com/Gyztor/NoTankControls";
+
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+		private static ModConfigurationKey<bool> MOD_ENABLED = new ModConfigurationKey<bool>("MOD_ENABLED", "Mod Enabled:", () => true);
 
         private static bool _first_trigger = false;
         public override void OnEngineInit()
         {
-            Harmony harmony = new Harmony("U-xyla.XyMod");
+            Harmony harmony = new Harmony("U-xyla.NoTankControls");
             harmony.PatchAll();
         }
 
         [HarmonyPatch(typeof(InteractionHandler))]
         [HarmonyPatch("BeforeInputUpdate")]
-        class Patch
+        class NoTankControlsPatch
         {
             private static void Postfix(InteractionHandler __instance, ref InteractionHandlerInputs ____inputs)
             {
-                ____inputs.Axis.RegisterBlocks = false;
+                if (Config.GetValue(MOD_ENABLED)) {
+                    ____inputs.Axis.RegisterBlocks = false;
+                }
             }
         }
     }
